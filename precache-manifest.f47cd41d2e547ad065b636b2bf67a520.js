74f820795919c4a5c15f98980fe570d45dd7b9b4
stadnikivan <stadnik11ivan@gmail.com>
2020-03-13 09:00:16 +0200

    Updates

---
+++ precache-manifest.f47cd41d2e547ad065b636b2bf67a520.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c3364c1c86daeaccaca785beb2ce2d",
+    "url": "/swrf/index.html"
+  },
+  {
+    "revision": "92d4f54d8a2b7a5a9f9e",
+    "url": "/swrf/static/css/main.70bd7104.chunk.css"
+  },
+  {
+    "revision": "d082423a8ee0cb0be53e",
+    "url": "/swrf/static/js/2.97876279.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/swrf/static/js/2.97876279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92d4f54d8a2b7a5a9f9e",
+    "url": "/swrf/static/js/main.0bd8aab0.chunk.js"
+  },
+  {
+    "revision": "ca05d4f036d069f6d43f",
+    "url": "/swrf/static/js/runtime-main.9ea86614.js"
+  }
+]);
